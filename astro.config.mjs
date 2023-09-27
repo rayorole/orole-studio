b9232bd70d14adc249d6c6d7ba21a60eea7a2468
Ray Orolé <orole.ray@protonmail.com>
2023-09-27 22:42:06 +0200

    Update pricing

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,62 +10,50 @@ import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://orole.studio",
-	vite: {
-		define: {
-			__DATE__: `'${new Date().toISOString()}'`,
-		},
-	},
-	integrations: [
-		tailwind(),
-		sitemap(),
-		astroI18next(),
-		alpinejs(),
-		AstroPWA({
-			mode: "development",
-			base: "/",
-			scope: "/",
-			includeAssets: ["favicon.svg"],
-			registerType: "autoUpdate",
-			manifest: {
-				name: "Orole - Your web development agency",
-				short_name: "Orole",
-				theme_color: "#ffffff",
-				icons: [
-					{
-						src: "pwa-192x192.png",
-						sizes: "192x192",
-						type: "image/png",
-					},
-					{
-						src: "pwa-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-					},
-					{
-						src: "pwa-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-				],
-			},
-			workbox: {
-				navigateFallback: "/404",
-				globPatterns: ["*.js"],
-			},
-			devOptions: {
-				enabled: true,
-				navigateFallbackAllowlist: [/^\/404$/],
-				suppressWarnings: true,
-			},
-		}),
-	],
-	markdown: {
-		rehypePlugins: [
-			rehypeSlug,
-			// This adds links to headings
-			[rehypeAutolinkHeadings, autolinkConfig],
-		],
-	},
+  site: "https://orole.studio",
+  vite: {
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`
+    }
+  },
+  integrations: [tailwind(), sitemap(), astroI18next(), alpinejs(), AstroPWA({
+    mode: "development",
+    base: "/",
+    scope: "/",
+    includeAssets: ["favicon.svg"],
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Orole - Your web development agency",
+      short_name: "Orole",
+      theme_color: "#ffffff",
+      icons: [{
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      }, {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+      }, {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable"
+      }]
+    },
+    workbox: {
+      navigateFallback: "/404",
+      globPatterns: ["*.js"]
+    },
+    devOptions: {
+      enabled: true,
+      navigateFallbackAllowlist: [/^\/404$/],
+      suppressWarnings: true
+    }
+  })],
+  markdown: {
+    rehypePlugins: [rehypeSlug,
+    // This adds links to headings
+    [rehypeAutolinkHeadings, autolinkConfig]]
+  }
 });
